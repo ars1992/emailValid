c3c448d13a52e5027fddfcd9af9627a7b04e28d5
ars1992 <alessandro.salomon@gmx.de>
2023-06-26 15:15:26 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner eingbe = new Scanner(System.in);
+        //String email = eingbe.nextLine();
+        String email = "aba@aa.de";
+
+        if(
+                !istStringNull(email) &&
+                !enthaeltLeerzeichen(email) &&
+                entaehltAt(email) &&
+                enthaeltDomain(email) &&
+                zeichenNachAT(email) >= 2 &&
+                zeichenVorAt(email) >= 3
+        ){
+            System.out.println("ok");
+        } else System.out.println("nicht ok");
+    }
+
+    private static boolean istStringNull(String email) throws NullPointerException{
+        if(email == null || email.isBlank())
+            throw new NullPointerException("ERROR: email ist leer");
+        return false;
+    }
+
+    private static boolean enthaeltDomain(String email){
+        String[] domains = {".de", ".com"};
+        for(String domain : domains){
+            if (email.endsWith(domain))
+                return true;
+        }
+        return false;
+    }
+
+    private static int zeichenVorAt(String email){
+        return email.split("@")[0].length();
+    }
+
+    private static int zeichenNachAT(String email){
+        String[] splitAt = email.split("@");
+        String stringNachAt = splitAt[splitAt.length - 1].split("\\.")[0];
+        return stringNachAt.length();
+    }
+
+    private static boolean entaehltAt(String email){
+        return email.contains("@");
+    }
+
+    private static boolean enthaeltLeerzeichen(String email){
+        return email.contains(" ");
+    }
+}
+
+
